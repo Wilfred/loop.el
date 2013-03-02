797e23936045108a5aedb5117d52806701f07ec6
Wilfred Hughes <me@wilfred.me.uk>
2013-03-02 19:28:06 +0000

    adding unit test for breaking out of a loop-while

--- loop-tests.el
+++ loop-tests.el
@@ -10,6 +10,18 @@
       (setq x (1+ x)))
     (should (equal sum 45))))
 
+(ert-deftest loop-test-while-break ()
+  (let ((x 0)
+        (sum 0))
+    ;; sum the numbers 0 to 5
+    (loop-while (< x 10)
+      (print sum)
+      (setq sum (+ sum x))
+      (setq x (1+ x))
+      (when (= x 6)
+        (loop-break)))
+    (should (equal sum 15))))
+
 (defun loop-run-tests ()
   (interactive)
   (ert-run-tests-interactively "loop-test-"))
