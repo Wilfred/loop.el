bd9f585fed51fe97a90f559b41abbfe0980a5c15
Wilfred Hughes <me@wilfred.me.uk>
2013-03-03 18:20:17 +0000

    adding a missing require

--- loop.el
+++ loop.el
@@ -37,6 +37,8 @@
 
 ;; loop-break, loop-continue, loop-return
 
+(require 'cl) ;; gensym
+
 (defmacro loop-while (condition &rest body)
   "Repeatedly evaluate BODY while CONDITION is non-nil."
   (declare (indent defun))
