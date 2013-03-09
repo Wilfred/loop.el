7ef52094f9d67609314344528b4692efa62c314c
Wilfred Hughes <me@wilfred.me.uk>
2013-03-09 14:12:53 +0000

    adding unit test of loop-break inside loop-for-each

--- loop-tests.el
+++ loop-tests.el
@@ -37,6 +37,15 @@
       (setq sum (+ sum x)))
     (should (equal sum 45))))
 
+(ert-deftest loop-test-for-each-break ()
+  (let ((sum 0))
+    ;; sum the numbers 1 to 5
+    (loop-for-each x (list 1 2 3 4 5 6 7 8 9)
+      (setq sum (+ sum x))
+      (when (= x 5)
+        (loop-break)))
+    (should (equal sum 15))))
+
 (defun loop-run-tests ()
   (interactive)
   (ert-run-tests-interactively "loop-test-"))
